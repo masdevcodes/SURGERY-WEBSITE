4259a90fd3ae42b6a85490765fb0c9080b03b955
masdevcodes <mas.dev.codes@gmail.com>
2025-09-17 21:36:32 +0530

    Updated providers.tsx

--- src/components/sections/providers.tsx
+++ src/components/sections/providers.tsx
@@ -182,7 +182,7 @@ export function Providers() {
 
                 <button
                   onClick={() => setSelectedProvider(provider)}
-                  className={`mt-4 w-full py-2 px-4 bg-${provider.color}-600 text-red rounded-xl hover:bg-${provider.color}-700 transition-colors duration-300`}
+                  className="px-6 py-2 bg-teal-600 text-white font-semibold rounded-full shadow-md hover:bg-teal-700 transition"
                 >
                   View More Deails of {provider.unit}
                 </button>
