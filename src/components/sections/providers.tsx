015e0d32aba9fcab97ddd1203298300519375c5e
masdevcodes <mas.dev.codes@gmail.com>
2025-09-18 10:26:27 +0530

    Updated providers.tsx

--- src/components/sections/providers.tsx
+++ src/components/sections/providers.tsx
@@ -51,7 +51,7 @@ export function Providers() {
       email: 'j.bowman@gmcpatiala.edu',
       description: 'Expert in respiratory medicine and critical care with extensive experience in treating lung diseases and sleep disorders.',
       color: 'blue',
-      image: '/images/singla.jpg',
+      image: '/images/aseem.png',
       details: {
         incharge: 'Dr. Prem Singla',
         assistantProfessors: ['Dr. Parth Dhamija', 'Dr. M'],
