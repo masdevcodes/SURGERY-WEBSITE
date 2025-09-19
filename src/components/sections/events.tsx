7be97bb6565f7c5e6ab063e67512512551058fc1
masdevcodes <mas.dev.codes@gmail.com>
2025-09-19 11:15:47 +0530

    Updated events.tsx

--- src/components/sections/events.tsx
+++ src/components/sections/events.tsx
@@ -105,7 +105,7 @@ export function Events() {
   const events = [
     {
       id: 2,
-      title: "Medical Workshop on Laparoscopic Surgery",
+      title: "Workshop on Robotic Laparoscopic Surgery",
       date: "February 28, 2024",
       images: [
         "https://images.unsplash.com/photo-1559757148-5c350d0d3c56?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80",
