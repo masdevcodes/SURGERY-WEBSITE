7375fc9241c3a27554cda3531983b14411181c95
masdevcodes <mas.dev.codes@gmail.com>
2025-09-19 23:49:43 +0530

    Updated providers.tsx

--- src/components/sections/providers.tsx
+++ src/components/sections/providers.tsx
@@ -65,7 +65,7 @@ export function Providers() {
       image: '/images/asem.png',
       details: {
         incharge: 'Dr. Prem Singla',
-        assistantProfessors: ['Dr. Parth Dhamija', 'Dr. M'],
+        assistantProfessors: ['Dr. P', 'Dr. M'],
         seniorResidents: ['Dr. S', 'Dr.', 'Dr. C'],
         juniorResidents: ['Dr. A', 'Dr. N', 'Dr. S', 'Dr. N', 'Dr. D', 'Dr. V', 'Dr. Y', 'Dr. P', 'Dr. S'],
       },
