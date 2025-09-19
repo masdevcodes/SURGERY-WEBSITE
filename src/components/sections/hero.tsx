fb746fa44e48c5e2ae424a405f12b426fd6d0f0c
masdevcodes <mas.dev.codes@gmail.com>
2025-09-19 12:08:33 +0530

    Updated hero.tsx

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -36,7 +36,7 @@ export function Hero() {
           <h1 className="text-8xl font-bold font-headline leading-tight">
             at your service
           </h1>
-          <h1 className="text-2xl font-bold font-headline leading-tight">
+          <h1 className="text-1xl font-bold font-headline leading-tight">
             DEPARTMENT OF GENERAL & MINIMAL ACCESS SURGERY, GMC PATIALA
           </h1>
         </div>
