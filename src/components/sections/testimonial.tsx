d8d082631bbfffb2a57eb79e6e739e23cba888c4
masdevcodes <mas.dev.codes@gmail.com>
2025-09-19 16:15:55 +0530

    Updated testimonial.tsx

--- src/components/sections/testimonial.tsx
+++ src/components/sections/testimonial.tsx
@@ -101,7 +101,7 @@ export function Testimonial() {
 
           {/* Subscribe Button */}
           <div className="flex items-center justify-center gap-4">
-            <img src="ytlogo.png" alt="YouTube Channel Logo" className="w-10 h-10 rounded-full" />
+            <img src="ytlogo.png" alt="YouTube Channel Logo" className="w-20 h-10 rounded-full" />
             <a
               href="https://www.youtube.com/@Scalpelsnsuture"
               target="_blank"
