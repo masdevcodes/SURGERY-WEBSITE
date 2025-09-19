7570ccfd0ad37ed99aafcd14b796c52ba8b950b7
masdevcodes <mas.dev.codes@gmail.com>
2025-09-19 12:32:49 +0530

    Updated about.tsx

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -37,7 +37,7 @@ export function About() {
               <div className="bg-white/10 backdrop-blur-md border border-white/20 p-6 rounded-lg shadow-lg hover:bg-white/15 hover:scale-105 transition-all duration-300 flex flex-col items-center text-center md:text-left md:items-start md:col-span-1">
                 <Stethoscope className="w-10 h-10 text-teal-500 mb-4" />
                 <h3 className="text-blue-950 font-bold font-body text-xl mb-2">Our Approach</h3>
-                <p className="text-sm text-blue-950/80">
+                <p className="text-sm text-justify text-blue-950/80">
                   For everyday care or life-changing care, you can count the patient on us to keep you and loved ones safe and healthy.
                 </p>
               </div>
