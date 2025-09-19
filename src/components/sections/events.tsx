2642c8cd2e28ed89548ecadba38e97714cd476d1
masdevcodes <mas.dev.codes@gmail.com>
2025-09-19 11:29:48 +0530

    Updated events.tsx

--- src/components/sections/events.tsx
+++ src/components/sections/events.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import Image from 'next/image';
+import Image from 'next/image/event';
 import { Calendar, ChevronLeft, ChevronRight, X } from 'lucide-react';
 import { useCallback, useEffect, useState } from 'react';
 import useEmblaCarousel from 'embla-carousel-react';
