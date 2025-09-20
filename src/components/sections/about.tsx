024ae125c9b7bd8f9fa7f8cbd3814e2c4a5df26d
masdevcodes <mas.dev.codes@gmail.com>
2025-09-20 08:23:36 +0530

    Updated about.tsx

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -29,9 +29,9 @@ export function About() {
             </div>
           </div>
           <div className="lg:col-span-3 space-y-12">
-            <h2 className="text-4xl font-bold text-blue-950 font-body leading-tight">
+            <h1 className="text-4xl font-bold text-blue-950 font-body leading-tight">
               For everyday care or life-changing care, you can count on us to keep you and your loved ones safe and healthy.
-            </h2>
+            </h1>
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
               <div className="bg-white/10 backdrop-blur-md border border-white/20 p-6 rounded-lg shadow-lg hover:bg-white/15 hover:scale-105 transition-all duration-300 flex flex-col items-center text-center md:text-left md:items-start md:col-span-1">
