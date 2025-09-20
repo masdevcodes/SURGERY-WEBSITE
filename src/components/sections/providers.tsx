f4b8cd66c2a692eb6368006cab122e3cd6e8f72b
masdevcodes <mas.dev.codes@gmail.com>
2025-09-20 09:32:21 +0530

    Updated providers.tsx

--- src/components/sections/providers.tsx
+++ src/components/sections/providers.tsx
@@ -292,7 +292,7 @@ export function Providers() {
                 />
               </div>
               <p className="text-gray-700 font-bold text-lg">
-                <strong><p classname="text-teal-500"></p>Unit Incharge:</strong> {selectedProvider.details.incharge}
+                <strong>UnitIncharge:</strong> {selectedProvider.details.incharge}
               </p>
             </div>
 
