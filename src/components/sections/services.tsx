025060f3471193d065fd9887af10f97c49abb578
masdevcodes <mas.dev.codes@gmail.com>
2025-09-20 08:57:30 +0530

    Updated services.tsx

--- src/components/sections/services.tsx
+++ src/components/sections/services.tsx
@@ -32,7 +32,7 @@ export function Services() {
         </div>
       ),
     },
-    {
+    //{
       icon: <Stethoscope className="w-8 h-8" />,
       title: 'Gastroenterology',
       description:
