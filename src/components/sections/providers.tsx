e676d910381472f8206f40aae35d122c3ad569a1
masdevcodes <mas.dev.codes@gmail.com>
2025-09-21 09:51:56 +0530

    Updated providers.tsx

--- src/components/sections/providers.tsx
+++ src/components/sections/providers.tsx
@@ -85,7 +85,7 @@ export function Providers() {
         incharge: 'Dr. H.S Rekhi',
         associateProfessors: [ ], // No  associate professors
         assistantProfessors: ['Dr. Sudesh Parthaph Singh', 'Dr. Malkiat Singh'],
-        seniorResidents: ['Dr. Smran Deep Singh', 'Dr. Baljeet Kaur'],
+        seniorResidents: ['Dr. Simran Deep Singh', 'Dr. Baljeet Kaur'],
         juniorResidents: ['Dr. Rajat Talresa', 'Dr. Samrat Singh Sra', 'Dr. Mohit Pareekh','Dr. Akhil Remesh','Dr. Geetanjli chopra','Dr. Shubham Chhabra','Dr. Bachittar Singh','Dr. Rishu Garg','Dr. Pardeep Bansal'],
       },
     },
