4f231c692737234505b3bfd2b7011ef72250cf8c
masdevcodes <mas.dev.codes@gmail.com>
2025-09-21 22:07:13 +0530

    Updated providers.tsx

--- src/components/sections/providers.tsx
+++ src/components/sections/providers.tsx
@@ -286,7 +286,7 @@ export function Providers() {
 
       <div className="container mx-auto relative z-10">
         <h2 className="text-6xl font-bold text-blue-950 font-headline mb-4">Our Providers</h2>
-        <p className="text-xl text-gray-600 mb-12">Meet our experienced Medical Professionals</p>
+        <p className="text-xl text-gray-600 mb-12">Meet  our experienced Medical Professionals</p>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {providers.map((provider) => {
