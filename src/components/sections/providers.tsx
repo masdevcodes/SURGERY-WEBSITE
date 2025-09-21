f6bda2ceef6cfbfa39077d8ba28f792089cab8e7
masdevcodes <mas.dev.codes@gmail.com>
2025-09-21 10:38:24 +0530

    Updated providers.tsx

--- src/components/sections/providers.tsx
+++ src/components/sections/providers.tsx
@@ -80,7 +80,7 @@ export function Providers() {
       email: 'a.hwang@gmcpatiala.edu',
       description: 'Leading urologist specializing in minimally invasive procedures and robotic surgery for uological conditions.',
       color: 'purple',
-      image: '/images/rehi.png',
+      image: '/images/rekhi.png',
       details: {
         incharge: 'Dr. H.S Rekhi',
         associateProfessors: [ ], // No  associate professors
