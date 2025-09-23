a0df578f7b84b510ab9fe38ea05631044540e9e7
masdevcodes <mas.dev.codes@gmail.com>
2025-09-23 09:13:18 +0530

    Updated medical-specialties.tsx

--- src/components/sections/medical-specialties.tsx
+++ src/components/sections/medical-specialties.tsx
@@ -139,7 +139,7 @@ export function MedicalSpecialties() {
                     <div className="flex flex-col items-center">
                       <div className="w-40 h-40 rounded-xl overflow-hidden shadow-md mb-4 group">
                         <Image
-                          src="/images/hod.png"
+                          src="/images/unit1/hod.png"
                           alt="Dr. Ashwani Kumar"
                           width={160}
                           height={160}
