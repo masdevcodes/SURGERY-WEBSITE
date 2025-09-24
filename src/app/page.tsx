e0b5b9962f73efc2c894f3cb3b967780f34c9ac0
masdevcodes <mas.dev.codes@gmail.com>
2025-09-24 09:39:31 +0530

    Roll back Visitors section changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,6 @@ import { History } from '@/components/sections/history';
 import { Events } from '@/components/sections/events';
 import { Contact } from '@/components/sections/contact';
 import { Footer } from '@/components/sections/footer';
-import { Visitors } from '@/components/sections/visitors';
 
 export default function Home() {
   return (
@@ -34,7 +33,6 @@ export default function Home() {
         <Achievements />
         <Events />
         <Testimonial />
-        <Visitors />
         <Contact />
       </main>
       <Footer />
