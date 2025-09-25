e40dd24e3ce84312ebf3ec35853270d9b6becc8a
masdevcodes <mas.dev.codes@gmail.com>
2025-09-25 16:26:43 +0530

    Updated about.tsx

--- src/components/sections/about.tsx
+++ src/components/sections/about.tsx
@@ -16,7 +16,7 @@ export function About() {
           <div className="lg:col-span-2">
             <div className="relative overflow-hidden rounded-lg">
               <Image
-                src="/kun.png"
+                src="/kun.webp"
                 alt="Doctors consulting with patient in hospital"
                 width={512}
                 height={1102}
