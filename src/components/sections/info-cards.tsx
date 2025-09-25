34768b829a075d16566f9a43f756de69075dd086
masdevcodes <mas.dev.codes@gmail.com>
2025-09-25 16:08:51 +0530

    Updated info-cards.tsx

--- src/components/sections/info-cards.tsx
+++ src/components/sections/info-cards.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import Image from 'next/images/infocard';
+import Image from 'next/image';
 import { ArrowRight, ChevronLeft, ChevronRight } from 'lucide-react';
 import { useState, MouseEvent, useEffect } from 'react';
 
