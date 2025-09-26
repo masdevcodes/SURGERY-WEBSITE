4aed22d335accbf04ff57d15649c75ae65963890
masdevcodes <mas.dev.codes@gmail.com>
2025-09-26 08:45:19 +0530

    Updated info-cards.tsx

--- src/components/sections/info-cards.tsx
+++ src/components/sections/info-cards.tsx
@@ -122,7 +122,7 @@ function getModalContent(key: string) {
           {/* Optimized Banner Image for Stoma Clinic */}
           <div className="w-full mb-6">
             <Image
-              src="/images/infocard/sto2.webp"
+              src="/images/infocard/popup/breppopup.jpg"
               alt="Stoma Clinic Banner"
               width={1200}
               height={400}
