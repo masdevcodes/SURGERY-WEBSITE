980e46206f3023a0f7bd9a6e3ea4046bd985e0ea
masdevcodes <mas.dev.codes@gmail.com>
2025-09-26 23:15:57 +0530

    Updated medical-specialties.tsx

--- src/components/sections/medical-specialties.tsx
+++ src/components/sections/medical-specialties.tsx
@@ -12,7 +12,7 @@ const unit1Data = {
   },
   associateProfessors: [
     { name: "Dr. Jaswinder Singh", img: "/images/unit1/jaswinder.webp" },
-    { name: "Dr. Dinesh Kumar Passi", img: "/images/unit1/dineshkumar.webp" },
+    { name: "Dr. Dinesh Kumar Passi", img: "/images/unit1/dinesh_kumar_passi.webp" },
   ],
   seniorResidents: [
     { name: "Dr. Parth Dhamija", img: "/images/unit1/parth.webp" },
