6c7e7d95f34990f82d7079d42d9119f479abf699
masdevcodes <mas.dev.codes@gmail.com>
2025-09-26 09:31:46 +0530

    Updated services.tsx

--- src/components/sections/services.tsx
+++ src/components/sections/services.tsx
@@ -162,7 +162,7 @@ export function Services() {
       title: 'Laparoscopic Cholecystectomy With CBD Exploration In A Patient With Situs Inversus Totalis',
       description: '',
       color: 'text-teal-500',
-      banner: '/images/super/ser1.webp',
+      banner: '/images/super/ser1.jpg',
       popupContent: (
         <div>
           <h3 className="font-bold text-2xl mb-4">Laparoscopic Cholecystectomy With CBD Exploration In A Patient With Situs Inversus Totalis</h3>
@@ -186,7 +186,7 @@ export function Services() {
       title: 'Endoscopic Thyroid Surgery via Axilla',
       description: '',
       color: 'text-teal-500',
-      banner: '/images/super/thyroid.webp',
+      banner: '/images/super/thyroid.jpg',
       popupContent: (
         <div>
           <h3 className="font-bold text-2xl mb-4">Endoscopic Thyroid Surgery via Axilla</h3>
@@ -208,7 +208,7 @@ export function Services() {
       title: 'Laparoscopic Adrenelectomy',
       description: '',
       color: 'text-teal-500',
-      banner: '/images/super/adrene.webp',
+      banner: '/images/super/adrene.jpg',
       popupContent: (
         <div>
           <h3 className="font-bold text-2xl mb-4">Laparoscopic Adrenelectomy</h3>
@@ -230,7 +230,7 @@ export function Services() {
       title: 'Laparoscopic Hysterectomy',
       description: '',
       color: 'text-teal-500',
-      banner: '/images/super/lah.webp',
+      banner: '/images/super/lah.jpg',
       popupContent: (
         <div>
           <h3 className="font-bold text-2xl mb-4">Laparoscopic Hysterectomy</h3>
@@ -252,7 +252,7 @@ export function Services() {
       title: 'Radio Frequency Ablation In Varicose Veins',
       description: '',
       color: 'text-teal-500',
-      banner: '/images/super/veins.webp',
+      banner: '/images/super/veins.jpg',
       popupContent: (
         <div>
           <h3 className="font-bold text-2xl mb-4">Radio Frequency Ablation In Varicose Veins</h3>
