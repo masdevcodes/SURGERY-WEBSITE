b95076be572631379c0a787308e513a09fc8aaca
masdevcodes <mas.dev.codes@gmail.com>
2025-09-26 10:33:37 +0530

    Updated info-cards.tsx

--- src/components/sections/info-cards.tsx
+++ src/components/sections/info-cards.tsx
@@ -559,9 +559,7 @@ function getModalContent(key: string) {
       {modalContent && (
         <Modal onClose={closeModal}>
           {imagesPreloaded && (
-            <div className="absolute top-4 left-4 bg-green-100 text-green-700 px-3 py-1 rounded-full text-xs font-bold">
-              ⚡ Lightning Fast
-            </div>
+            
           )}
           {getModalContent(modalContent)}
         </Modal>
