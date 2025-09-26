cfc513e28b2122cbb3e6f668a404d37212965bb3
masdevcodes <mas.dev.codes@gmail.com>
2025-09-26 09:28:58 +0530

    Updated services.tsx

--- src/components/sections/services.tsx
+++ src/components/sections/services.tsx
@@ -161,7 +161,7 @@ export function Services() {
       title: 'Laparoscopic Cholecystectomy With CBD Exploration In A Patient With Situs Inversus Totalis',
       description: '',
       color: 'text-teal-500',
-      banner: '/images/super/ser1.jpg',
+      banner: '/images/super/ser1.webp',
       popupContent: (
         <div>
           <h3 className="font-bold text-2xl mb-4">Laparoscopic Cholecystectomy With CBD Exploration In A Patient With Situs Inversus Totalis</h3>
@@ -185,7 +185,7 @@ export function Services() {
       title: 'Endoscopic Thyroid Surgery via Axilla',
       description: '',
       color: 'text-teal-500',
-      banner: '/images/super/thyroid.jpg',
+      banner: '/images/super/thyroid.webp',
       popupContent: (
         <div>
           <h3 className="font-bold text-2xl mb-4">Endoscopic Thyroid Surgery via Axilla</h3>
@@ -202,7 +202,7 @@ export function Services() {
         </div>
       ),
     },
-    { 
+    {
       icon: <Bone className="w-8 h-8" />,
       title: 'Laparoscopic Adrenelectomy',
       description: '',
