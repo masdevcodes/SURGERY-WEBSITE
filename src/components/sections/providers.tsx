48e30781b7e7d947fff976fe2ff51939d23a1454
masdevcodes <mas.dev.codes@gmail.com>
2025-09-27 09:33:50 +0530

    Updated providers.tsx

--- src/components/sections/providers.tsx
+++ src/components/sections/providers.tsx
@@ -398,6 +398,7 @@ export function Providers() {
                   alt={selectedProvider.details.incharge}
                   width={128}
                   height={128}
+                  quality={70}
                   className="object-cover w-full h-full transition-transform duration-500 group-hover:scale-110"
                 />
               </div>
